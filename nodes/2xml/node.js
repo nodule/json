2a850383885b7340ede02315b60055ccada9cdd5
Rob Halff <rob.halff@gmail.com>
2014-01-30 23:04:59 +0100

    beautify

--- nodes/2xml/node.js
+++ nodes/2xml/node.js
@@ -1,11 +1,13 @@
 var obj = {};
-if(input.root) {
+if (input.root) {
   obj[input.root] = input.json;
 } else {
   obj = input.json;
 }
 
-output.out = json2xml(obj, { header: input.header });
+output.out = json2xml(obj, {
+  header: input.header
+});
 
 /*
 on.input.root = function() {

e4fb5c4b671b184d7eeec98510a3625eee930240
Rob Halff <rob.halff@gmail.com>
2014-01-30 23:05:02 +0100

    beautify

--- nodes/parse/node.js
+++ nodes/parse/node.js
@@ -1,11 +1,11 @@
 /* output.out = JSON.parse(input.in, input.reviver); */
 
-on.input.in = function() {
+on.input. in = function () {
   output({
     out: JSON.parse(data, state.reviver)
   });
 };
 
-on.input.reviver = function() {
+on.input.reviver = function () {
   state.reviver = data;
 };

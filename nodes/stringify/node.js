e2ca8240fc5099f0618cd6931f0bfb4ebea54e41
Rob Halff <rob.halff@gmail.com>
2014-02-22 13:17:13 +0100

    make stringify async

--- nodes/stringify/node.js
+++ nodes/stringify/node.js
@@ -1,1 +1,8 @@
-output.out = JSON.stringify(input. in , input.replacer, input.space);
+//output.out = JSON.stringify(input. in , input.replacer, input.space);
+on.input.in = function() {
+
+  output({
+    out: JSON.stringify(data, input.replacer, input.space);
+  });
+
+};
